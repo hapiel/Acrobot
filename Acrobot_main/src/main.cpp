ecd56d6e1d036931e4b687e5e4d2b531b61a4468
Hapiel <daniel@danielsiegmund.nl>
2023-12-04 16:13:00 +0100

    Webserver prepared

--- Acrobot_main/src/main.cpp
+++ Acrobot_main/src/main.cpp
@@ -27,8 +27,7 @@ The project should be built in platformio
 #include <Arduino.h>
 #include "Wire.h"
 #include "SPI.h"
-#include "SdFat.h"
-#include "sdios.h"
+#include "SD.h"
 #include "RemoteDebug.h" //https://github.com/JoaoLopesF/RemoteDebug , using fork: https://github.com/karol-brejna-i/RemoteDebug
 #include "WiFi.h"
 #include "DNSServer.h"
@@ -38,7 +37,6 @@ The project should be built in platformio
 #include "ItemToggle.h"
 #include "ItemCommand.h"
 #include "LcdMenu.h" // needs to be after item-imports!!!
-#include "SPI.h"
 #include "WebServer.h"
 #define DBG_OUTPUT_PORT Serial
 
@@ -74,7 +72,7 @@ The project should be built in platformio
 #define BUTTON_RIGHT 32
 #define BATTERY_SENSOR 36
 
-#define HOST_NAME "Acrobot"
+#define HOST_NAME "acrobot"
 #define SCK 22
 #define MISO 19
 #define MOSI 23
@@ -86,10 +84,7 @@ LiquidCrystal_I2C lcd(0x27, 20, 4); // 20 wide 4 tall
 LcdMenu lcdMenu(3, 20);
 // need custom SPI class because of incorrect wiring
 SPIClass spi = SPIClass(VSPI);
-#define SD_CONFIG SdSpiConfig(CS, DEDICATED_SPI, SD_SCK_MHZ(16), &spi)
-// only allow exFAT sd, can be changed, see library.
-SdExFat sd;
-ExFile file;
+
 
 // custom libraries
 Joystick joystick;
@@ -326,6 +321,158 @@ bool loadFromSdCard(String path) {
   return true;
 }
 
+void handleFileUpload() {
+  if (server.uri() != "/edit") {
+    return;
+  }
+  HTTPUpload& upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    if (SD.exists((char *)upload.filename.c_str())) {
+      SD.remove((char *)upload.filename.c_str());
+    }
+    uploadFile = SD.open(upload.filename.c_str(), FILE_WRITE);
+    DBG_OUTPUT_PORT.print("Upload: START, filename: "); DBG_OUTPUT_PORT.println(upload.filename);
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    if (uploadFile) {
+      uploadFile.write(upload.buf, upload.currentSize);
+    }
+    DBG_OUTPUT_PORT.print("Upload: WRITE, Bytes: "); DBG_OUTPUT_PORT.println(upload.currentSize);
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (uploadFile) {
+      uploadFile.close();
+    }
+    DBG_OUTPUT_PORT.print("Upload: END, Size: "); DBG_OUTPUT_PORT.println(upload.totalSize);
+  }
+}
+
+
+void deleteRecursive(String path) {
+  File file = SD.open((char *)path.c_str());
+  if (!file.isDirectory()) {
+    file.close();
+    SD.remove((char *)path.c_str());
+    return;
+  }
+
+  file.rewindDirectory();
+  while (true) {
+    File entry = file.openNextFile();
+    if (!entry) {
+      break;
+    }
+    String entryPath = path + "/" + entry.name();
+    if (entry.isDirectory()) {
+      entry.close();
+      deleteRecursive(entryPath);
+    } else {
+      entry.close();
+      SD.remove((char *)entryPath.c_str());
+    }
+    yield();
+  }
+
+  SD.rmdir((char *)path.c_str());
+  file.close();
+}
+
+void handleDelete() {
+  if (server.args() == 0) {
+    return returnFail("BAD ARGS");
+  }
+  String path = server.arg(0);
+  if (path == "/" || !SD.exists((char *)path.c_str())) {
+    returnFail("BAD PATH");
+    return;
+  }
+  deleteRecursive(path);
+  returnOK();
+}
+
+void handleCreate() {
+  if (server.args() == 0) {
+    return returnFail("BAD ARGS");
+  }
+  String path = server.arg(0);
+  if (path == "/" || SD.exists((char *)path.c_str())) {
+    returnFail("BAD PATH");
+    return;
+  }
+
+  if (path.indexOf('.') > 0) {
+    File file = SD.open((char *)path.c_str(), FILE_WRITE);
+    if (file) {
+      file.write(0);
+      file.close();
+    }
+  } else {
+    SD.mkdir((char *)path.c_str());
+  }
+  returnOK();
+}
+
+void printDirectory() {
+  if (!server.hasArg("dir")) {
+    return returnFail("BAD ARGS");
+  }
+  String path = server.arg("dir");
+  if (path != "/" && !SD.exists((char *)path.c_str())) {
+    return returnFail("BAD PATH");
+  }
+  File dir = SD.open((char *)path.c_str());
+  path = String();
+  if (!dir.isDirectory()) {
+    dir.close();
+    return returnFail("NOT DIR");
+  }
+  dir.rewindDirectory();
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+  server.send(200, "text/json", "");
+
+  server.sendContent("[");
+  for (int cnt = 0; true; ++cnt) {
+    File entry = dir.openNextFile();
+    if (!entry) {
+      break;
+    }
+
+    String output;
+    if (cnt > 0) {
+      output = ',';
+    }
+
+    output += "{\"type\":\"";
+    output += (entry.isDirectory()) ? "dir" : "file";
+    output += "\",\"name\":\"";
+    output += entry.path();
+    output += "\"";
+    output += "}";
+    server.sendContent(output);
+    entry.close();
+  }
+  server.sendContent("]");
+  dir.close();
+}
+
+void handleNotFound() {
+  if (hasSD && loadFromSdCard(server.uri())) {
+    return;
+  }
+  String message = "SDCARD Not Detected\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " NAME:" + server.argName(i) + "\n VALUE:" + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  DBG_OUTPUT_PORT.print(message);
+}
+
+
 
 // webserver end
 
@@ -339,6 +486,7 @@ void initDebug()
 
 void inits()
 {
+
   Serial.begin(115200); // Initialize Serial for USB communication
 
   Serial.println("Next init: wifi");
@@ -357,12 +505,13 @@ void inits()
   spi.begin(SCK, MISO, MOSI, CS);
 
   debugI("Next init: SD card");
-  if (!sd.begin(SD_CONFIG))
+  if (!SD.begin(CS))
   {
     debugE("SD card init FAILED!");
   }
   else
   {
+    hasSD = true; //webserver
     debugI("SD card init done.");
   }
 
@@ -390,6 +539,20 @@ void inits()
   debugI("Next init: Menu");
   menu.init(mainMenu);
 
+
+  // webserver
+  server.on("/list", HTTP_GET, printDirectory);
+  server.on("/edit", HTTP_DELETE, handleDelete);
+  server.on("/edit", HTTP_PUT, handleCreate);
+  server.on("/edit", HTTP_POST, []() {
+    returnOK();
+  }, handleFileUpload);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  // webserver end
+
+
   debugI("Inits Done.");
   buzzer.buzz(40); // short buzz to indicate boot
 }
@@ -406,6 +569,7 @@ void wifiConnection()
         debugI("MDNS responder started. Hostname -> %s or %s.local", HOST_NAME, HOST_NAME);
       }
       MDNS.addService("telnet", "tcp", 23);
+      MDNS.addService("http", "tcp", 80);
       debugI("Connected to IP address: %s ", WiFi.localIP().toString().c_str());
     }
   }
@@ -432,6 +596,9 @@ void updates()
   menu.update();
   statusChecker.update();
   choreoPlayer.update();
+
+  // webserver
+  server.handleClient();
 }
 
 void updatesI2C()
