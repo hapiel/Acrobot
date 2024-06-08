8290b074b2c9b2d0217a7cca765cb6801b90967f
Ricard Mallafre <rikitzzz@gmail.com>
2024-06-08 21:21:07 +0200

    feat: show limbs' statuses on website dashboard

---
+++ acrobot_dashboard/src/components/limb/Limb.tsx
@@ -0,0 +1,54 @@
+import { LimbStatus } from '@/lib/AcroborAPI';
+import { cn } from '@/lib/utils';
+
+export type LimbProps = {
+  status?: LimbStatus;
+  children: React.ReactNode;
+};
+export function Limb({ status, children }: LimbProps) {
+  return (
+    <div
+      className={cn(
+        'rounded-lg border',
+        status?.isOnline ? 'border-green-600 dark:border-green-600' : 'border-red-600 dark:border-red-600'
+      )}
+    >
+      <div className="flex flex-col">
+        {children}
+        <div className="rounded-b-lg *:px-2">
+          <div className="even:bg-gray-600/50">
+            <h2 className="font-bold">Position</h2>
+            {status?.isOnline ? <p>{status?.position ?? '-'}</p> : <p>-</p>}
+          </div>
+          <div className="even:bg-gray-600/50">
+            <h2 className="font-bold">Velocity</h2>
+            {status?.isOnline ? <p>{status?.velocity ?? '-'}</p> : <p>-</p>}
+          </div>
+          <div className="even:bg-gray-600/50">
+            <h2 className="font-bold">Torque</h2>
+            {status?.isOnline ? <p>{status?.torque ?? '-'}</p> : <p>-</p>}
+          </div>
+          <div className="even:bg-gray-600/50">
+            <h2 className="font-bold">Temperature</h2>
+            {status?.isOnline ? <p>{status?.temperature ?? '-'}</p> : <p>-</p>}
+          </div>
+          <div className="even:bg-gray-600/50">
+            <h2 className="font-bold">Status</h2>
+            <p className={cn(status?.isOnline ? 'text-green-600' : 'text-red-600')}>
+              {status?.isOnline ? 'Online' : 'Offline'}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export type LimbNameProps = {
+  children: React.ReactNode;
+};
+
+export function LimbName({ children }: LimbNameProps) {
+  return (
+    <h1 className="rounded-t-lg bg-gray-800/60 p-2 text-xl font-bold uppercase dark:bg-gray-200/30">{children}</h1>
+  );
+}
--- acrobot_dashboard/src/components/ui/dropdown-menu.tsx
+++ acrobot_dashboard/src/components/ui/dropdown-menu.tsx
@@ -79,7 +79,7 @@ const DropdownMenuItem = React.forwardRef<
   <DropdownMenuPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-stone-100 focus:text-stone-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-stone-800 dark:focus:text-stone-50',
+      'relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-stone-100 focus:text-stone-900 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-stone-800 dark:focus:text-stone-50',
       inset && 'pl-8',
       className
     )}
