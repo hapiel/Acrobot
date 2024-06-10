0bdc3577b46a8eb4aaa355a4763a0b1b407dc2ec
Ricard Mallafre <rikitzzz@gmail.com>
2024-06-10 17:01:08 +0200

    feat: use zustand to keep track of robot status globally

--- acrobot_dashboard/src/App.tsx
+++ acrobot_dashboard/src/App.tsx
@@ -1,27 +1,30 @@
 import { Limb, LimbName } from '@/components/limb/Limb';
 import { Button } from '@/components/ui/button';
-import { GetStatusResponse, acrobot } from '@/lib/AcroborAPI';
-import { useState } from 'react';
+import { useRobotStatus } from '@/hooks/useRobotState';
+import { acrobot } from '@/lib/AcroborAPI';
 
 function App() {
-  const [response, setResponse] = useState<Partial<GetStatusResponse>>({});
+  const {
+    status: { rightArm, rightLeg, leftArm, leftLeg },
+    setStatus
+  } = useRobotStatus();
 
   return (
     <main className="flex min-w-full flex-col items-center justify-center pt-2">
       <div className="flex flex-col">
-        <Button onClick={async () => setResponse(await acrobot.getStatus())}>Get status</Button>
+        <Button onClick={async () => setStatus(await acrobot.getStatus())}>Get status</Button>
       </div>
       <div className="grid w-full grid-cols-2 grid-rows-2 gap-4 p-2">
-        <Limb status={response.rightArm}>
+        <Limb status={rightArm}>
           <LimbName>Right arm</LimbName>
         </Limb>
-        <Limb status={response.leftArm}>
+        <Limb status={leftArm}>
           <LimbName>Left arm</LimbName>
         </Limb>
-        <Limb status={response.rightLeg}>
+        <Limb status={rightLeg}>
           <LimbName>Right leg</LimbName>
         </Limb>
-        <Limb status={response.leftLeg}>
+        <Limb status={leftLeg}>
           <LimbName>Left leg</LimbName>
         </Limb>
       </div>
---
+++ acrobot_dashboard/src/hooks/useRobotState.ts
@@ -0,0 +1,35 @@
+import { GetStatusResponse } from '@/lib/AcroborAPI';
+import { create } from 'zustand';
+
+export type State = {
+  status: GetStatusResponse;
+  setStatus: (status: GetStatusResponse) => void;
+  setRightArm: (status: GetStatusResponse['rightArm']) => void;
+  setLeftArm: (status: GetStatusResponse['leftArm']) => void;
+  setRightLeg: (status: GetStatusResponse['rightLeg']) => void;
+};
+
+export const useRobotStatus = create<State>((set) => ({
+  status: {} as GetStatusResponse,
+  setStatus: (status: GetStatusResponse) => set({ status }),
+  setRightArm: (status: GetStatusResponse['rightArm']) => {
+    set((state) => {
+      return { status: { ...state.status, rightArm: status } };
+    });
+  },
+  setLeftArm: (status: GetStatusResponse['leftArm']) => {
+    set((state) => {
+      return { status: { ...state.status, leftArm: status } };
+    });
+  },
+  setRightLeg: (status: GetStatusResponse['rightLeg']) => {
+    set((state) => {
+      return { status: { ...state.status, rightLeg: status } };
+    });
+  },
+  setLeftLeg: (status: GetStatusResponse['leftLeg']) => {
+    set((state) => {
+      return { status: { ...state.status, leftLeg: status } };
+    });
+  }
+}));
