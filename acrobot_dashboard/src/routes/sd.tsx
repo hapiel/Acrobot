1534e5caddd225a81a96bf7923830986c3e19f9a
Ricard Mallafre <rikitzzz@gmail.com>
2024-08-19 19:00:49 +0200

    feat: save file changes

--- acrobot_dashboard/src/routes/sd.tsx
+++ acrobot_dashboard/src/routes/sd.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/lib/utils';
 import axios from 'axios';
-import { File, Folder, Loader2, Play, Repeat, RotateCcw } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { File as FileIcon, Folder, Loader2, Play, Repeat, RotateCcw, Save } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
 import { useQuery } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
@@ -16,6 +16,7 @@ export function SD() {
     keepPreviousData: true
   });
   const fileContents = useQuery(['file_contents', fileName], ({ queryKey }) => fetchFileContents(queryKey[1]));
+  const pre = useRef<HTMLPreElement>(null);
 
   useEffect(() => {
     const handlePopState = (event: PopStateEvent) => {
@@ -56,45 +57,61 @@ export function SD() {
                 key={type + name}
                 className={cn(
                   fileName === name ? 'bg-stone-500' : '',
-                  'flex justify-between gap-2 border-b-2 border-gray-700 pl-1 transition-all hover:cursor-pointer hover:bg-stone-400'
+                  'flex items-center justify-between gap-2 border-b-2 border-gray-700 pl-1 transition-all hover:cursor-pointer hover:bg-stone-400'
                 )}
                 onClick={() => (type === 'dir' ? setPath(name) : setFileName(name))}
               >
-                <div className="flex items-center justify-center gap-2">
-                  {type === 'dir' ? <Folder /> : <File />} {name}
+                <div className="flex w-[450px] items-center">
+                  <div className="flex items-center overflow-x-auto py-1">
+                    {type === 'dir' ? <Folder size={26} /> : <FileIcon size={26} />}{' '}
+                    <span className="ml-2">{name}</span>
+                  </div>
                 </div>
                 <div
                   className={cn(
-                    'flex items-center justify-center gap-2 bg-stone-800 p-1',
-                    type === 'dir' ? 'pointer-events-none invisible' : ''
+                    type === 'dir' ? 'pointer-events-none invisible' : '',
+                    'flex h-[100%] items-center justify-center bg-stone-800'
                   )}
                 >
                   <Play
                     onClick={() => playFile({ file: name, mode: 'once', power: 50 })}
-                    size={30}
-                    className="rounded-md p-1 transition-all hover:cursor-pointer hover:bg-stone-600"
+                    size={32}
+                    className="h-[100%] p-1 transition-all hover:cursor-pointer hover:bg-stone-600"
                   />
                   <Repeat
                     onClick={() => playFile({ file: name, mode: 'repeat', power: 50 })}
-                    size={30}
-                    className="rounded-md p-1 transition-all hover:cursor-pointer hover:bg-stone-600"
+                    size={32}
+                    className="h-[100%] p-1 transition-all hover:cursor-pointer hover:bg-stone-600"
                   />
                   <RotateCcw
                     onClick={() => playFile({ file: name, mode: 'beginPosOnly', power: 50 })}
-                    size={30}
-                    className="rounded-md p-1 transition-all hover:cursor-pointer hover:bg-stone-600"
+                    size={32}
+                    className="h-[100%] p-1 transition-all hover:cursor-pointer hover:bg-stone-600"
                   />
                 </div>
               </div>
             );
           })}
         </div>
-        <div className="relative max-w-[100%] flex-grow overflow-x-auto border-l-2 border-gray-200">
-          <h3 className="sticky left-0 bg-gray-700 p-2 text-center">{fileName}</h3>
+        <div className="relative flex-grow overflow-x-auto border-l-2 border-gray-200">
+          <div className="sticky left-0 bg-gray-700 p-2 text-center">
+            <h3>{fileName}</h3>
+            <div className="absolute right-2 top-1 hover:cursor-pointer">
+              <Save
+                onClick={() => {
+                  const fileContents = pre.current?.innerText;
+                  if (fileContents) saveChanges({ fileName, fileContents });
+                }}
+                size={32}
+              />
+            </div>
+          </div>
           {fileContents.isLoading ? (
             <Loader2 className="animate-spin" />
           ) : (
-            <pre className="m-2">{fileContents.data ? fileContents.data : null}</pre>
+            <pre ref={pre} contentEditable="plaintext-only" className="m-2 focus-visible:outline-none">
+              {fileContents.data ? fileContents.data : null}
+            </pre>
           )}
         </div>
       </div>
@@ -129,9 +146,21 @@ export type PlayFileOptions = {
   mode: 'once' | 'repeat' | 'beginPosOnly';
   power: number;
 };
+
 async function playFile({ file, mode, power }: PlayFileOptions) {
   await axios.get('/play', {
     baseURL: 'http://acrobot.local',
     params: { file, mode, power }
   });
+}
+
+export type SaveChangeOptions = {
+  fileName: string;
+  fileContents: string;
+};
+
+async function saveChanges({ fileName, fileContents }: SaveChangeOptions) {
+  const form = new FormData();
+  form.append('data', new File([new Blob([fileContents])], fileName));
+  await axios.post('/edit', form, { baseURL: 'http://acrobot.local' });
 }
